6ee0f9ba95a66e351ede4fc39a009a44508aa606
felister kiragu <kiragufelister18@gmail.com>
2024-09-27 15:06:32 +0300

    add index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function() {
+    return 2000 / 100; 
+  };
+  
+  // 3) Arrow function called square
+  const square = (num) => num * num;  // 4) Takes one parameter and multiplies it by itself
+  
+  // 5) Arrow function called add
+  const add = (a, b) => a + b;  // 6) Takes two parameters and adds them together
+  
+  // Example usage:
+  console.log(divide());   // Output: 20
+  console.log(square(5));  // Output: 25
+  console.log(add(4, 6));  
